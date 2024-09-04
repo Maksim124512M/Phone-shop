4fc39c395f0fa4695007fc56dbba115a68bb73d0
Maksim124512M <max1996az@gmail.com>
2024-09-04 19:48:32 +0300

    Users and logout page

--- mobile_shop/urls.py
+++ mobile_shop/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('phones.urls')),
+    path('', include('phones.urls', namespace='phones')),
+    path('accounts/', include('users.urls', namespace='users')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- phones/urls.py
+++ phones/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from phones.views import HomeView, PhoneDetailView
 
 
+app_name = 'phones'
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('phone/<int:pk>/', PhoneDetailView.as_view(), name='phone_detail'),
